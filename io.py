881abe4ad37b114fcf4f86d0790f30ee43dfc891
Yerson sanchez <37232721+yerson001@users.noreply.github.com>
2021-03-07 00:03:51 -0500

    Update io.py

--- io.py
+++ io.py
@@ -1,82 +1,119 @@
-import RPi.GPIO as GPIO          
-from time import sleep
+#!/usr/bin/python3
+# Web Del código:
+# http://python4fun.blogspot.com/2008/06/get-key-press-in-python.html
+import termios, sys, os
+TERMIOS = termios
+SI=1
+NO=0
 
-in1 = 24
-in2 = 23
-en = 25
-temp1=1
+def getkey():
+	fd = sys.stdin.fileno()
+	old = termios.tcgetattr(fd)
+	new = termios.tcgetattr(fd)
+	new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
+	new[6][TERMIOS.VMIN] = 1
+	new[6][TERMIOS.VTIME] = 0
+	termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
+	c = None
+	try:
+		c=str(os.read(fd,4))
+	finally:
+		termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
+	c_Longitud=len(c)
+	if(c=="b'\\x1b'"):
+		c="[ESC]"
+	if(c=="b'\\x1b[A'"):
+		c="[CURSOR_ARRIBA]"
+	if(c=="b'\\x1b[B'"):
+		c="[CURSOR_ABAJO]"
+	if(c=="b'\\x1b[D'"):
+		c="[CURSOR_IZQUIERDA]"
+	if(c=="b'\\x1b[C'"):
+		c="[CURSOR_DERECHA]"
+	if(c=="b'\\n'"):
+		c="[INTRO]"
+	if(c=="b'\\t'"):
+		c="[TAB]"
+	#print(c,"          ") # Para Latinoamerica
+	if(c=="b'\\x1bOP'"):
+		c="[F1]"
+	if(c=="b'\\x1bOQ'"):
+		c="[F2]"
+	if(c=="b'\\x1bOR'"):
+		c="[F3]"
+	if(c=="b'\\x1bOS'"):
+		c="[F4]"
+	if(c=="b'\\x1b[15'"):
+		c="[F5]"
+	if(c=="b'\\x1b[17'"):
+		c="[F6]"
+	if(c=="b'\\x1b[18'"):
+		c="[F7]"
+	if(c=="b'\\x1b[19'"):
+		c="[F8]"
+	if(c=="b'\\x1b[20'"):
+		c="[F9]"
+	if(c=="b'\\x1b[21'"):
+		c="[F10]"
+	if(c=="b'\\x1b[23'"):
+		c="[F11]"
+	if(c=="b'\\x1b[24'"):
+		c="[F12]"
+	if(c=="b'\\x7f'"):
+		c="[BORRAR]"
+	if(c=="b'\\x1b[3~'"):
+		c="[SUPRIMIR]"
+	if(c=="b'\\x1b[H'"):
+		c="[INICIO]"
+	if(c=="b'\\x1b[F'"):
+		c="[FIN]"
+	if(c=="b'\\x1b[5~'"):
+		c="[RePag]"
+	if(c=="b'\\x1b[6~'"):
+		c="[AvPag]"
+	if(c=="b'\\x1b[2~'"):
+		c="[INSERTAR]"
+	if(c=="b'\\xc3\\x91'"):
+		c="[Ñ]"
+	if(c=="b'\\xc3\\xb1'"):
+		c="[ñ]"
+	if(c=="b'\\xc2\\xbf'"):
+		c="[¿]"
+	if(c=="b'\\xc2\\xa1'"):
+		c="[¡]"
+	if(c=="b'\\xc3\\xa7'"):
+		c="[ç]"
+	if(c=="b'\\xc3\\x87'"):
+		c="[Ç]"
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
-p.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
-
-while(1):
-
-    x=raw_input()
+	#print(c_Longitud)
     
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         print("forward")
-         x='z'
-        else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         print("backward")
-         x='z'
+	if(c_Longitud==4):
+		c="["+c[2]+"]"
 
+	if(c=="[ ]"):
+		c="[ESPACIO]"
+        
+	return c
+	
+Ejecutar=SI
+print("\033c\033[3J") # Limpia La Pantalla De La Terminal
+print("\033[?25l") # Oculta El Cursor
+print("\033[0;0H", end="") # Coloca el cursor al inicio
+print("Pulsa una tecla, pulsa [ESC] para SALIR")
+#os.system("tput civis") 
 
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
+while(Ejecutar==SI):
+	Tecla=getkey()
+	print("\033[0;0H", end="")
+	print(Tecla)
+	if(Tecla=="[ESC]"):
+		Ejecutar=NO
+	if(Tecla=="[w]"):
+		print("wwwww")
+		
 
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
-
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+print("Pulsa una tecla para finalizar...")
+Tecla=getkey()
+print("\033[?25h")	# Show the cursor
+quit()
